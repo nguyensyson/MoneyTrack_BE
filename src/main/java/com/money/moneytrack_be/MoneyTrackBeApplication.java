9f8830411a4a296a1dc52963d33c19d61af3da2c
nguyensyson <ssnguyenson5671@gmail.com>
2026-03-29 10:20:02 +0700

    init local

---
+++ src/main/java/com/money/moneytrack_be/MoneyTrackBeApplication.java
@@ -0,0 +1,13 @@
+package com.money.moneytrack_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyTrackBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoneyTrackBeApplication.class, args);
+    }
+
+}
